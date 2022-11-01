43d60149a49c17a02545ed3d2f80cdbcb7e0ec57
Ulviyyaaa <tu7s25j8q@code.edu.az>
2022-11-02 01:51:28 +0400

    task

---
+++ task1/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class task
+{
+    static void Main(string[] args)
+    {
+        int[] ededler = { -6, 3, 9, 15, 18, 30 };
+        int cem = 0;
+
+        for (int i = 0; i < ededler.Length; i++)
+        {
+            if (ededler[i] > 0 && ededler[i] % 6 == 0)
+            {
+                Console.WriteLine(ededler[i]);
+                cem += ededler[i];
+            }
+        }
+        Console.WriteLine("cem :" + cem);
+
+    }
+}
+
---
+++ task2 c#/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿class task2
+{
+    static void Main()
+    {
+        List<long> list = new List<long> { 150, 300, 400, 350, 450, 550, 600 };
+        foreach (long ele in list)
+        {
+            Console.WriteLine(ele);
+        }
+
+        long max_num = list.AsQueryable().Max();
+
+
+        Console.WriteLine("Largest number = {0}", max_num);
+    }
+}
